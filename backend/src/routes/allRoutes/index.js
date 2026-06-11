ef0e5d40fb8d0d2dc758b902b8cea6932ac70714
David <2017lasvegasedu@gmail.com>
2026-06-11 00:11:06 -0600

    fix: arreglar estructura de carpetas y por eso daba error al iniciar

--- backend/src/routes/allRoutes/index.js
+++ backend/src/routes/allRoutes/index.js
@@ -1,14 +1,10 @@
 import { Router } from "express";
-import adminRoutes from "../adminRoutes.js";
 import suppliersRoutes from "../suppliersRoutes.js";
 import productsRoutes from "../productsRoutes.js";
 // Aquí importamos todas las rutas de cada módulo
-
-import adminRoutes from "../adminRoutes.js";
 const router = Router();
 
 //Nombres de los endpoints
-router.use("/admin", adminRoutes);
 router.use("/suppliers", suppliersRoutes);
 router.use("/products", productsRoutes);
 
--- backend/src/utils/cloudinaryConfig.js
+++ backend/src/utils/cloudinaryConfig.js
@@ -1,7 +1,7 @@
 import multer from "multer";
 import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
-import { config } from "../config.js";
+import { config } from "../../config.js";
 
 //Configuramos cloudinary con nuestras credenciales
 cloudinary.config({
